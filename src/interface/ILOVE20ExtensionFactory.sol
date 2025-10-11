714fbe6725408843dbabbd4be44632ccca4b1adc
BigPolarBear <hanxincade@gmail.com>
2025-10-12 02:45:33 +0800

    add exists() to Factory

--- src/interface/ILOVE20ExtensionFactory.sol
+++ src/interface/ILOVE20ExtensionFactory.sol
@@ -1,10 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.17;
 interface ILOVE20ExtensionFactory {
-    // ------ Center should not trust, only for reference ------
-    function name() external view returns (string memory);
-    function version() external view returns (string memory);
-
     // ------ created extensions, may not be initialized ------
     function extensionsCount(
         address tokenAddress
@@ -14,4 +10,6 @@ interface ILOVE20ExtensionFactory {
         address tokenAddress,
         uint256 index
     ) external view returns (address);
+
+    function exists(address extension) external view returns (bool);
 }
