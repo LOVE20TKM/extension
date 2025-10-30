f18b50c7db1d4875e093284fee169f8502b6af03
BigPolarBear <hanxincade@gmail.com>
2025-10-30 11:12:17 +0800

    feat: add internal references to core protocol contracts in LOVE20ExtensionBase
    - Import core protocol interfaces (Launch, Stake, Submit, Vote, Verify, Mint, Random)
    - Add immutable state variables to store contract references
    - Initialize all core contracts in constructor via extension center

--- src/LOVE20ExtensionBase.sol
+++ src/LOVE20ExtensionBase.sol
@@ -5,7 +5,14 @@ import {ILOVE20ExtensionCenter} from "./interface/ILOVE20ExtensionCenter.sol";
 import {ILOVE20Extension} from "./interface/ILOVE20Extension.sol";
 import {ILOVE20ExtensionFactory} from "./interface/ILOVE20ExtensionFactory.sol";
 import {ILOVE20Token} from "@love20/interfaces/ILOVE20Token.sol";
+import {ILOVE20Launch} from "@love20/interfaces/ILOVE20Launch.sol";
+import {ILOVE20Stake} from "@love20/interfaces/ILOVE20Stake.sol";
+import {ILOVE20Submit} from "@love20/interfaces/ILOVE20Submit.sol";
+import {ILOVE20Vote} from "@love20/interfaces/ILOVE20Vote.sol";
 import {ILOVE20Join} from "@love20/interfaces/ILOVE20Join.sol";
+import {ILOVE20Verify} from "@love20/interfaces/ILOVE20Verify.sol";
+import {ILOVE20Mint} from "@love20/interfaces/ILOVE20Mint.sol";
+import {ILOVE20Random} from "@love20/interfaces/ILOVE20Random.sol";
 
 uint256 constant DEFAULT_JOIN_AMOUNT = 1000000000000000000; // 1 token
 
@@ -29,6 +36,23 @@ abstract contract LOVE20ExtensionBase is ILOVE20Extension {
     /// @notice Whether the extension has been initialized
     bool public initialized;
 
+    /// @notice The launch contract address
+    ILOVE20Launch internal immutable _launch;
+    /// @notice The stake contract address
+    ILOVE20Stake internal immutable _stake;
+    /// @notice The submit contract address
+    ILOVE20Submit internal immutable _submit;
+    /// @notice The vote contract address
+    ILOVE20Vote internal immutable _vote;
+    /// @notice The join contract address
+    ILOVE20Join internal immutable _join;
+    /// @notice The verify contract address
+    ILOVE20Verify internal immutable _verify;
+    /// @notice The mint contract address
+    ILOVE20Mint internal immutable _mint;
+    /// @notice The random contract address
+    ILOVE20Random internal _random;
+
     /// @dev Array of accounts participating in this extension
     address[] internal _accounts;
 
@@ -51,6 +75,17 @@ abstract contract LOVE20ExtensionBase is ILOVE20Extension {
     /// @param factory_ The factory contract address
     constructor(address factory_) {
         factory = factory_;
+        ILOVE20ExtensionCenter c = ILOVE20ExtensionCenter(
+            ILOVE20ExtensionFactory(factory_).center()
+        );
+        _launch = ILOVE20Launch(c.launchAddress());
+        _stake = ILOVE20Stake(c.stakeAddress());
+        _submit = ILOVE20Submit(c.submitAddress());
+        _vote = ILOVE20Vote(c.voteAddress());
+        _join = ILOVE20Join(c.joinAddress());
+        _verify = ILOVE20Verify(c.verifyAddress());
+        _mint = ILOVE20Mint(c.mintAddress());
+        _random = ILOVE20Random(c.randomAddress());
     }
 
     // ============================================
